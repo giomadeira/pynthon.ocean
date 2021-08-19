6246aead4b877c83be0347cd0af544289c5cdd5e
Giovana Madeira <giomadeirag@gmail.com>
2021-08-19 23:32:10 +0000

    inclusao banco de dados

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
-# 1ª linha ta importando o Flask - que e um framework.
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
+
+#configuraçao 
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
+
 # variavel para guardar o arquivo
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
 
 @app.route('/hello')
 def pagina_inicial():
