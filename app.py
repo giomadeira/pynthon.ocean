003ace5c786e831fb919a0052b5ef3c9718cd4f3
Giovana Madeira <giomadeirag@gmail.com>
2021-08-19 23:58:09 +0000

    inclusao das rotas

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ app.config.from_object(__name__)
 def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
 
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
 
+@app.teardown_request
+def depois_request(exc):
+    g.bd.close()
+
+@app.route('/')
+@app.route('/entradas')
+def exibir_entradas():
+    return render_template('exibir_entradas.html')   
 
 @app.route('/hello')
 def pagina_inicial():
