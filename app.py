962323a8d24a5f9db34f6b2d0002ef5135726c32
Giovana Madeira <giomadeirag@gmail.com>
2021-08-27 00:41:13 +0000

    inclusão login, logout e layout

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, abort, render_template, flash
+from flask import Flask, request, url_for, session, g, redirect, abort, render_template, flash
 
 
 #configuraçao 
@@ -21,6 +21,7 @@ def antes_requisicao():
 def depois_request(exc):
     g.bd.close()
 
+
 @app.route('/')
 @app.route('/entradas')
 def exibir_entradas():
@@ -31,7 +32,7 @@ def exibir_entradas():
         entradas.append({'titulo': titulo, 'texto': texto})
     return render_template('exibir_entradas.html', entradas=entradas)   
 
-@app.route('/inserir')
+@app.route('/inserir' , methods=["POST"])
 def inserir_entradas():
     if not session.get('logado'):
         abort(401)
@@ -39,3 +40,27 @@ def inserir_entradas():
     g.bd.execute(sql, request.form['campoTitulo'], request.form['campoTexto'])
     g.bd.commit()
     return redirect(url_for('exibir_entradas'))                    
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    return redirect(url_for('exibir_entradas'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['campoUsuario'] != 'admin' \
+        or request.form['campoSenha'] != 'admin':
+            erro = "Senha ou Usuário Inválidos"
+        else:
+            session['logado'] = True
+            return redirect(url_for('exibir_entradas'))
+
+    return render_template('login.html', erro=erro)
